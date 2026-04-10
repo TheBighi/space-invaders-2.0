ea187e99197793359442f8d393a8b83259985574
BiggieCheese <thebighix@gmail.com>
2026-04-10 13:13:07 +0300

    added enemies spwaning

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 #define BULLETS_TOTAL 255
-#define ENEMIES_TOTAL 100
+#define ENEMIES_TOTAL 10000
 #define BULLET_RADIUS 5
 #define ENEMY_RADIUS 15
 
@@ -17,30 +17,33 @@
 const int screenWidth = 800;
 const int screenHeight = 450;
 
-int bulletIndice = 0;
-int enemyIndice = 0;
+Vector2 emptyVector = {0,0};
+
+int timer = 0;
 
 Vector2 bullets[BULLETS_TOTAL] = { 0 };
 
 Vector2 enemies[ENEMIES_TOTAL] = { 0 };
 
 void insertIntoBullets(Vector2 ballPos){
-        Vector2 pos = { ballPos.x, ballPos.y };
-        if (bulletIndice >= BULLETS_TOTAL) {
-            return;
+        for (int i = 0; i < BULLETS_TOTAL; i++) {
+            if (bullets[i].x == emptyVector.x && bullets[i].y == emptyVector.y) {
+                Vector2 pos = { ballPos.x, ballPos.y };
+                bullets[i] = pos;
+                return;
+            }
         }
-        bullets[bulletIndice] = pos;
-        bulletIndice++;
 }
 
 void insertEnemy(){
-    if (enemyIndice >= ENEMIES_TOTAL) {
-        return;
+    for (int i = 0; i < ENEMIES_TOTAL; i++) {
+        if (enemies[i].x == emptyVector.x && enemies[i].y == emptyVector.y) {
+            int randomX = rand() % 800;
+            int randomY = rand() % 200 + 100;
+            enemies[i] = (Vector2){ .x = randomX, .y = randomY};
+            return;
+        }
     }
-    int randomX = rand() % 800;
-    int randomY = rand() % 200 + 100;
-    enemies[enemyIndice] = (Vector2){ .x = randomX, .y = randomY};
-    enemyIndice ++;
 }
 
 int main(void){
@@ -56,7 +59,6 @@ int main(void){
     Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 + 180 };
     Vector2 middlePos =  { (float)screenWidth/2, (float)screenHeight/2 };
 
-    Vector2 emptyVector = {0,0};
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------\
@@ -64,13 +66,19 @@ int main(void){
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key if u want to exit please 
-    {
+    {   
+
+        timer++;
+
         if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 5.0f;
         if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 5.0f;
 
         if (IsKeyPressed(KEY_SPACE)) insertIntoBullets(ballPosition);
 
-        if (IsKeyDown(KEY_T)) insertEnemy();
+        if (timer == 100) {
+            insertEnemy();
+            timer = 0;
+        }
 
         if (ballPosition.x < 20.f) {
             ballPosition.x = 20;
@@ -80,19 +88,19 @@ int main(void){
             ballPosition.x = 780;
         }
 
-        for (int i = 0; i < bulletIndice; i++) {
+        for (int i = 0; i < BULLETS_TOTAL; i++) {
             bullets[i].y -= 5.0f; 
         }
 
-        for (int i = bulletIndice - 1; i >= 0; i--) {
+        for (int i = BULLETS_TOTAL - 1; i >= 0; i--) {
             if (bullets[i].y <= 0){
                 bullets[i] = emptyVector;
             }
         }
 
 
-        for (int i = 0; i < bulletIndice; i++) {
-            for (int j = 0; j < enemyIndice; j++) {
+        for (int i = 0; i < BULLETS_TOTAL; i++) {
+            for (int j = 0; j < ENEMIES_TOTAL; j++) {
                 float E_BDistX = enemies[j].x - bullets[i].x;
                 float E_BDisty = enemies[j].y - bullets[i].y;
                 float distTotal = sqrtf(E_BDisty * E_BDisty + E_BDistX * E_BDistX);
@@ -104,13 +112,12 @@ int main(void){
         }
 
         printf("%f, %f \n", enemies[0].x, enemies[0].y);
-        printf("%d \n", enemyIndice);
 
         BeginDrawing();
 
             ClearBackground(BLACK);
 
-            DrawText("Use right arrow and lkeft arrow to move.", 190, 200, 20, WHITE);
+            DrawText("Use right arrow and left arrow to move.", 200, 10, 20, WHITE);
 
             DrawCircleV(ballPosition, 20, GREEN);
 
